915cdafc9c362f3c4781ea7b1bfc4252e819ca8c
Felipe Bastos <felipe.bastos@terra.com.br>
2020-01-18 16:57:23 -0500

    fix indentation, wrap lines at 80 chars

--- threadpool.h
+++ threadpool.h
@@ -1,41 +1,39 @@
 #pragma once
 
-#include <condition_variable>
-#include <functional> //bind
-#include <future>     //packaged_task
-#include <mutex>      //unique_lock
-#include <queue>
-#include <thread>
-#include <type_traits> //invoke_result, enable_if, is_invocable
-#include <vector>
-
-#include "globals.h"
+#include <condition_variable> //condition_variable
+#include <future>             //packaged_task
+#include <mutex>              //unique_lock
+#include <queue>              //queue
+#include <thread>             //thread
+#include <type_traits>        //invoke_result, enable_if, is_invocable
+#include <vector>             //vector
 
 class thread_pool {
 public:
     thread_pool(size_t thread_count);
     ~thread_pool();
 
-    // since std::thread objects are not copiable, it doesn't make sense for a
-    // thread_pool
-    //  to be copiable.
+    //since std::thread objects are not copiable, it doesn't make sense for a
+    //  thread_pool to be copiable.
     thread_pool(const thread_pool &) = delete;
     thread_pool &operator=(const thread_pool &) = delete;
 
     template <typename F, typename... Args,
-            std::enable_if_t<std::is_invocable_v<F &&, Args &&...>, int> = 0>
+        std::enable_if_t<std::is_invocable_v<F &&, Args &&...>, int> = 0
+    >
     auto execute(F &&, Args &&...);
 
 private:
     //_task_container_base and _task_container exist simply as a wrapper around a
     //  MoveConstructible - but not CopyConstructible - Callable object. Since an
-    //  std::function requires a given Callable to be CopyConstructible, we cannot
-    //  construct one from a lambda function that captures a non-CopyConstructible
-    //  object (such as the packaged_task declared in execute) - because a lambda
-    //  capturing a non-CopyConstructible object is not CopyConstructible.
+    //  std::function requires a given Callable to be CopyConstructible, we
+    //  cannot construct one from a lambda function that captures a
+    //  non-CopyConstructible object (such as the packaged_task declared in
+    //  execute) - because a lambda capturing a non-CopyConstructible object is
+    //  not CopyConstructible.
 
     //_task_container_base exists only to serve as an abstract base for
-    //_task_container.
+    //  _task_container.
     class _task_container_base {
     public:
         virtual ~_task_container_base(){};
@@ -45,17 +43,17 @@ private:
 
     //_task_container takes a typename F, which must be Callable and
     //MoveConstructible.
-    //  Furthermore, F must be callable with no arguments; it can, for example, be
-    //  a bind object with no placeholders. F may or may not be CopyConstructible.
+    //  Furthermore, F must be callable with no arguments; it can, for example,
+    //  be a bind object with no placeholders. F may or may not be
+    //  CopyConstructible.
     template <typename F,
         std::enable_if_t<std::is_invocable_v<F&&>, int> = 0
     >
     class _task_container : public _task_container_base {
     public:
-        // here, std::forward is needed because we need the construction of _f *not*
-        // to
-        //  bind an lvalue reference - it is not a guarantee that an object of type
-        //  F is CopyConstructible, only that it is MoveConstructible.
+        //here, std::forward is needed because we need the construction of _f not
+        //  to bind an lvalue reference - it is not a guarantee that an object of
+        //  type F is CopyConstructible, only that it is MoveConstructible.
         _task_container(F &&func) : _f(std::forward<F>(func)) {}
 
         void operator()() override { _f(); }
@@ -64,20 +62,24 @@ private:
         F _f;
     };
 
-    // returns a unique_ptr to a _task_container that wraps around a given
-    // function
-    //  for details on _task_container_base and _task_container, see above
-    //  This exists so that _Func may be inferred from f.
+    //returns a unique_ptr to a _task_container that wraps around a Callable
+    //  for details on _task_container_base and _task_container, see above. This
+    //  exists so that _Func may be inferred from f.
+    //
+    //  Note: currently, compiler support is bad for deduction guides. However,
+    //        in a perfect world, `allocate_task_container` can be completely
+    //        replaced by adding a deduction guide to the `_task_container` ctor:
+    //        _task_container(F&&) -> _task_container<std::decay<F>>
     template <typename _Func>
     static std::unique_ptr<_task_container_base>
     allocate_task_container(_Func &&f) {
-    // in the construction of the _task_container, f must be std::forward'ed
-    // because
-    //  it may not be CopyConstructible - the only requirement for an
+    //in the construction of the _task_container, f must be std::forward'ed
+    //  because it may not be CopyConstructible - the only requirement for an
     //  instantiation of a _task_container is that the parameter is of a
     //  MoveConstructible type.
-    return std::unique_ptr<_task_container_base>(
-        new _task_container<_Func>(std::forward<_Func>(f)));
+        return std::unique_ptr<_task_container_base>(
+	        new _task_container<_Func>(std::forward<_Func>(f))
+	    );
     }
 
     std::vector<std::thread> _threads;
